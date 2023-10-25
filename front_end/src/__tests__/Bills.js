c304d738981dbaa656b6dcee8bed4f5ad1576f3b
Nicolas DECRESSAC <decressac.nicolas@icloud.com>
2023-10-25 15:24:24 +0200

    Adding tests on Bills.js
    — package.json :
    
       - add package whatwg-fetch to front-end (node-fetch wasn't working with Node 18.18.2 on Linux)
    
    — Bills.js :
       - add get Bills testing functions

--- front_end/src/__tests__/Bills.js
+++ front_end/src/__tests__/Bills.js
@@ -2,48 +2,103 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom";
+import 'whatwg-fetch' // https://www.npmjs.com/package/whatwg-fetch 'cause node-fetch isn't working
+import {screen, waitFor} from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
-import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
-import { localStorageMock } from "../__mocks__/localStorage.js";
-
+import {bills} from "../fixtures/bills.js";
+import {ROUTES_PATH} from "../constants/routes.js";
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import mockStore from '../__mocks__/store.js'
 import router from "../app/Router.js";
 
-// import formatDate, for testing with the same format as returned in bills.js
-import {formatDate} from "../app/format.js";
+
+jest.mock('../app/Store.js', () => mockStore)
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
-      Object.defineProperty(window, "localStorage", {
-        value: localStorageMock,
-      });
-      window.localStorage.setItem(
-        "user",
-        JSON.stringify({
-          type: "Employee",
-        })
-      );
-      const root = document.createElement("div");
-      root.setAttribute("id", "root");
-      document.body.append(root);
-      router();
-      window.onNavigate(ROUTES_PATH.Bills);
-      await waitFor(() => screen.getByTestId("icon-window"));
-      const windowIcon = screen.getByTestId("icon-window");
-      //to-do write expect expression
+    describe("When I am on Bills Page", () => {
+        test("Then bill icon in vertical layout should be highlighted", async () => {
+            Object.defineProperty(window, "localStorage", {
+                value: localStorageMock,
+            });
+            window.localStorage.setItem("user", JSON.stringify({
+                type: "Employee",
+            }));
+            const root = document.createElement("div");
+            root.setAttribute("id", "root");
+            document.body.append(root);
+            router();
+            window.onNavigate(ROUTES_PATH.Bills);
+            await waitFor(() => screen.getByTestId("icon-window"));
+            const windowIcon = screen.getByTestId("icon-window");
+            expect(windowIcon.classList.contains('active-icon')).toBe(true)
+        });
+        test("Then bills should be ordered from earliest to latest", () => {
+            document.body.innerHTML = BillsUI({data: bills});
+            const dates = screen
+                .getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i)
+                .map((a) => a.innerHTML);
+            const antiChrono = (a, b) => (a < b ? 1 : -1);
+            const datesSorted = dates.sort(antiChrono);
+            expect(dates).toEqual(datesSorted);
+        });
     });
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills });
-      const dates = screen
-        .getAllByText(
-          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
-        )
-        .map((a) => a.innerHTML);
-      const antiChrono = (a, b) => (a < b ? 1 : -1);
-      const datesSorted = dates.sort(antiChrono);
-      expect(dates).toEqual(datesSorted);
-    });
-  });
 });
+
+// test integration Get bills
+describe(`Given i'm a user connected as an employee`, () => {
+    describe(`When i navigate to Bills page`, () => {
+        test(`fetches bills from mock API GET`, async () => {
+            Object.defineProperty(window, 'localStorage', {value: localStorageMock})
+            localStorage.setItem('user', JSON.stringify({type: 'Employee', email: 'a@a'}));
+            const root = document.createElement('div')
+            root.setAttribute('id', 'root')
+            document.body.append(root)
+            router()
+            window.onNavigate(ROUTES_PATH.Bills)
+            await waitFor(() => expect(screen.getByText('Mes notes de frais')).toBeTruthy())
+        })
+        describe('When an error occurs on API', () => {
+            beforeEach(() => {
+                jest.spyOn(mockStore, 'bills')
+                Object.defineProperty(
+                    window,
+                    'localStorage',
+                    {value: localStorageMock}
+                )
+                window.localStorage.setItem('user', JSON.stringify({
+                    type: 'Employee',
+                    email: 'a@a'
+                }))
+                const root = document.createElement('div')
+                root.setAttribute('id', 'root')
+                document.body.appendChild(root)
+                router()
+            })
+            test('fetches bills from an API and fails with 404 message error', async () => {
+                mockStore.bills.mockImplementationOnce(() => {
+                    return {
+                        list: () => {
+                            return Promise.reject(new Error('Erreur 404'))
+                        }
+                    }
+                })
+                window.onNavigate(ROUTES_PATH.Bills)
+                await new Promise(process.nextTick)
+                await expect(screen.getByText(/Erreur 404/)).toBeTruthy()
+            })
+            test('Fetches messages from an API and fails with 500 message error', async () => {
+                mockStore.bills.mockImplementationOnce(() => {
+                    return {
+                        list : () => {
+                            return Promise.reject(new Error('Erreur 500'))
+                        }
+                    }
+                })
+
+                window.onNavigate(ROUTES_PATH.Bills)
+                await new Promise(process.nextTick)
+                expect(await screen.getByText(/Erreur 500/)).toBeTruthy()
+            })
+        })
+    })
+})
