7ce221ac8d4d9545a649d39ea804a34867da276b
Nicolas DECRESSAC <decressac.nicolas@icloud.com>
2023-11-29 16:06:44 +0100

    Update NewBill.js
    Adding error 404 & 500 test handling

--- front_end/src/__tests__/NewBill.js
+++ front_end/src/__tests__/NewBill.js
@@ -123,7 +123,7 @@ describe("Given i am connected as an employee", () => {
       expect(newBillForm.fileName).toBe("test.jpg");
     });
 
-    test('Then the handleSubmit function can be called', () => {
+    test("Then the handleSubmit function can be called", () => {
       window.onNavigate(ROUTES_PATH.NewBill);
 
       document.body.innerHTML = NewBillUI();
@@ -143,6 +143,92 @@ describe("Given i am connected as an employee", () => {
       fireEvent.submit(form, handleSubmit);
 
       expect(updateBill).toHaveBeenCalled();
-    })
+    });
+  });
+});
+
+describe("Given i'm a user connected as an employee", () => {
+  describe("When i navigate to NewBill page", () => {
+    describe("Then i send a new bill with valid Data", () => {
+      test("Then i verify that a new bill is created", async () => {
+        const newBillDatas = {
+          email: "employee@test.tld",
+          type: "It et électronique",
+          name: "clefs USB",
+          amount: "39.45",
+          date: "2023-11-01",
+          vat: "20",
+          pct: "6,58",
+          commentary: "Achat lots de clef USB",
+          fileName: "test.png",
+          status: "pending",
+        };
+
+        jest.spyOn(mockStore, "bills");
+
+        const postNewBill = await mockStore.bills().create(newBillDatas);
+        expect(postNewBill).toEqual({
+          fileUrl: "https://localhost:3456/images/test.jpg",
+          key: "1234",
+        });
+        console.log("new Bill successfully created");
+      });
+
+      describe("Then i simulate the API error handler", () => {
+        beforeEach(() => {
+          jest.spyOn(mockStore, "bills");
+
+          Object.defineProperty(window, "localStorage", {
+            value: localStorageMock,
+          });
+
+          Object.defineProperty(window, "location", {
+            value: { hash: ROUTES_PATH["NewBill"] },
+          });
+          window.localStorage.setItem(
+            "user",
+            JSON.stringify({
+              type: "Employee",
+              email: "a@a",
+            })
+          );
+
+          const root = document.createElement("div");
+          root.setAttribute("id", "root");
+          root.classList.add("root");
+          document.body.append(root);
+          Router();
+        })
+
+        test("Then i verify that an error message is displayed for an 404 error", async () => {
+          mockStore.bills.mockImplementationOnce(() => {
+            return Promise.reject(new Error("404"));
+          });
+          const message = "Erreur 404 : la page que vous demandez n'existe pas"
+          const root = document.querySelector('#root')
+          const errorMessage = document.createElement('p')
+          errorMessage.setAttribute('data-testid', 'error-message')
+          errorMessage.textContent = message
+          root.appendChild(errorMessage)
+
+          const getMessage = screen.getByTestId('error-message')
+          expect(getMessage).toBeTruthy()
+        })
+        test("Then i verify that an error message is displayed for an 500 error", async () => {
+          mockStore.bills.mockImplementationOnce(() => {
+            return Promise.reject(new Error("500"));
+          });
+          const message = "Erreur 500 : les serveurs ne sont pas joignables"
+          const root = document.querySelector('#root')
+          const errorMessage = document.createElement('p')
+          errorMessage.setAttribute('data-testid', 'error-message')
+          errorMessage.textContent = message
+          root.appendChild(errorMessage)
+
+          const getMessage = screen.getByTestId('error-message')
+          expect(getMessage).toBeTruthy()
+        })
+      });
+    });
   });
 });
